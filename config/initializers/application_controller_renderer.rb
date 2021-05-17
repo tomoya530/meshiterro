7b61f967f7ea1cacba81076232bc5556321203f2
tomoya530 <ec2-user@ip-172-31-15-226.ec2.internal>
2021-05-17 06:14:09 +0000

    [Finish] app2-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0125eb983bae11b78c99a203a00982d184b31d8fd86d4535e56d06e9b0c1f3512e3083b32c80a4e84c4a362da6b1c9110026ea38ff2d13409112c7dcdfd9cd87'
